cf7229f88c51c899a477a350870afc6a4e7c2cfa
snupa <bunta.adrian@gmail.com>
2017-02-10 09:37:16 +0200

    version bump

--- index.js
+++ index.js
@@ -13,11 +13,35 @@
  *    NOTE2: IF the UIAM Cache app is present, we will use websockets for a faster and persistent connection, so
  *    make sure that your back-end can support websockets.
  */
+const Client = require('uiamp-client');
 module.exports = (thorin, opt, pluginName) => {
   opt = thorin.util.extend({
     logger: pluginName || 'iam',
-
+    url: '',  // Your UIAMP Server URL
+    key: ''   // Your UIAMP Server Key generated with "npm run token" in the server
   }, opt);
+  let pluginObj = {},
+    clientObj,
+    logger = thorin.logger(opt.logger);
+  try {
+    clientObj = new Client(opt);
+    clientObj.run = (done) => {
+      clientObj.connect().then(() => {
+        logger.trace(`Connected to UIAMP`);
+        done();
+      }).catch((e) => {
+        logger.trace(`Could not initiate UIAMP client`);
+        return done(thorin.error('IAM.CONNECT', e.message || 'Could not connect to UIAMP'));
+      });
+    }
+  } catch (e) {
+    clientObj = {};
+    clientObj.run = (done) => {
+      return done(thorin.error('IAM.CONNECT', 'Missing UIAMP server URL'));
+    };
+  }
+
+  return clientObj;
 };
 
 module.exports.publicName = 'iam';
